0008383fb479a5d166c1414e69c326791536d649
clementpnn <clementpnn@users.noreply.github.com>
2025-01-31 02:24:31 +0100

    change work directory

--- apps/frontend/src/services/utils/instance.ts
+++ apps/frontend/src/services/utils/instance.ts
@@ -2,9 +2,6 @@ import ky from "ky"
 
 const kyInstance = ky.create({
   prefixUrl: "http://localhost:3000/api/v1",
-  headers: {
-    "Authorization": `Bearer ${import.meta.env.VITE_TOKEN}`
-  }
 })
 
 export default kyInstance
